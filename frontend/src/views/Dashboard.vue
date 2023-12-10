f1361257d13875497f0084f0ece99cbb6a27e767
nichakan <nichakan4740@gmail.com>
2023-12-11 03:24:03 +0700

    add_commit

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -316,7 +316,7 @@ const closeModal = () => {
           </div>
           
                       <button
-                       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                       class="ml-9 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                       >
                         แก้ไขค่าระดับน้ำตาล
                       </button>
--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -7,12 +7,13 @@ import { useRouter } from "vue-router";
 </script>
  
 <template>
+   <div class="container  ml-20 mr-20 mt-20 ">
+   <router-link :to="{ name: 'home' }">
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+  เข้าใช้งานระบบ</button>
+</router-link>
 
-   
-
-<router-link :to="{ name: 'home' }">home</router-link>
-
-
+</div>
 
 
 
